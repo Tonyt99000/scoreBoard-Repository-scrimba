4d2b9e53014415abeda5eeaf26b1da57163c5719
Tonyt <113797163+Tonyt99000@users.noreply.github.com>
2023-02-11 00:12:21 -0500

    added borders to elements

--- index.js
+++ index.js
@@ -3,18 +3,18 @@
 let homeStore = document.getElementById("home-score")
 //let homeScoreBtnOne = document.getElementById("home-score-btn-1")
 let homeScore = 0
-function increaseHomeScoreOne(){
+function add1H(){
     homeScore = homeScore + 1
     homeStore.textContent = homeScore   
 }
 //HOME BUTTON #2
 //let homeScoreBtnTwo = document.getElementById("home-score-btn-2")
-function increaseHomeScoreTwo(){
+function add2H(){
     homeScore = homeScore + 2
     homeStore.textContent = homeScore
 }
 //HOME BUTTON #3
-function increaseHomeScoreThree(){
+function add3H(){
     homeScore = homeScore + 3
     homeStore.textContent = homeScore
 }
@@ -23,17 +23,17 @@ function increaseHomeScoreThree(){
 let awayStore = document.getElementById("away-score")
 let awayScore = 0
 
-function add1(){
+function add1A(){
     awayScore = awayScore + 1
     awayStore.textContent = awayScore
 }
 //AWAY BUTTON #2
-function add2(){
+function add2A(){
     awayScore = awayScore + 2
     awayStore.textContent = awayScore
 }
 //AWAY BUTTON #3
-function add3(){
+function add3A(){
     awayScore = awayScore + 3
     awayStore.textContent = awayScore
 }
